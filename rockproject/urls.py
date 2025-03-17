ba40f7e562a68cb17aa8ee36a3e4164f98cf7ea7
Nate Z <ntzeitlin@gmail.com>
2025-03-17 13:27:25 -0500

    type view functioning

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rockapi.views import register_user, login_user
+from rockapi.views import register_user, login_user, TypeView
 
 router = routers.DefaultRouter(trailing_slash=False)
+# Pattern for below: URL, View to Use, base name (nickname for the resource used in errors)
+router.register(r"types", TypeView, "type")
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('register', register_user),
-    path('login', login_user),
-    path('admin/', admin.site.urls),
+    path("", include(router.urls)),
+    path("register", register_user),
+    path("login", login_user),
+    path("admin/", admin.site.urls),
 ]

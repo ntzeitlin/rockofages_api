48fbece2c54b0ca9803516c157c420375831f0dc
Nate Z <ntzeitlin@gmail.com>
2025-03-17 14:11:19 -0500

    returned rocks now properly expand data

---
+++ rockapi/views/rock_view.py
@@ -0,0 +1,63 @@
+from django.http import HttpResponseServerError
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from rockapi.models import Rock, Type
+from django.contrib.auth.models import User
+
+
+class RockView(ViewSet):
+    """Rock view set"""
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized instance
+        """
+
+        # You will implement this feature in a future chapter
+        return Response("", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def list(self, request):
+        """Handle GET requests for all items
+
+        Returns:
+            Response -- JSON serialized array
+        """
+        try:
+            rocks = Rock.objects.all()
+            serializer = RockSerializer(rocks, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+
+class RockTypeSerializer(serializers.ModelSerializer):
+    """JSON serializer"""
+
+    class Meta:
+        model = Type
+        fields = ("label",)
+
+
+class RockUserSerializer(serializers.ModelSerializer):
+    """JSON serializer"""
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+        )
+
+
+class RockSerializer(serializers.ModelSerializer):
+    """JSON serializer"""
+
+    type = RockTypeSerializer(many=False)
+    user = RockUserSerializer(many=False)
+
+    class Meta:
+        model = Rock
+        fields = ("id", "name", "weight", "user", "type")
